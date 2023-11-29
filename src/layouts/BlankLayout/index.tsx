0432df775ee1e3cecc0c2c036cee40c3f17f1204
zhangming <qianzm2490@gmail.com>
2023-11-29 15:31:24 +0800

    fix: 布局组件中渲染路由内容方式修改

--- src/layouts/BlankLayout/index.tsx
+++ src/layouts/BlankLayout/index.tsx
@@ -1,5 +1,7 @@
+import { Outlet } from '@umijs/max';
+
 import type { FC, PropsWithChildren } from 'react';
 
-const BlankLayout: FC<PropsWithChildren> = ({ children }) => <>{children}</>;
+const BlankLayout: FC<PropsWithChildren> = () => <Outlet />;
 
 export default BlankLayout;
--- src/layouts/WindowLayout/index.tsx
+++ src/layouts/WindowLayout/index.tsx
@@ -1,17 +1,21 @@
+import { Outlet } from '@umijs/max';
+
 import LayoutHeader from '@/components/LayoutHeader';
 
 import styles from './index.less';
 
 import type { FC, PropsWithChildren } from 'react';
 
-const WindowLayout: FC<PropsWithChildren> = ({ children }) => {
+const WindowLayout: FC<PropsWithChildren> = () => {
   return (
     <div className={styles.container}>
       <div className={styles.header}>
         <LayoutHeader />
       </div>
       <div className={styles.main}>
-        <div className={styles.content}>{children}</div>
+        <div className={styles.content}>
+          <Outlet />
+        </div>
       </div>
     </div>
   );
